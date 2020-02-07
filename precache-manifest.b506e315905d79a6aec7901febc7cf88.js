5191997defa4eafd4b6812ecd62e38368ba5ee12
kim young jo <kimyoungjo@kimui-MacBook-Pro.local>
2020-02-07 17:08:01 +0900

    Updates

---
+++ precache-manifest.b506e315905d79a6aec7901febc7cf88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc7143a6a9aa865c94cfe95fed270f4",
+    "url": "/kimyoungjo-portfolio/index.html"
+  },
+  {
+    "revision": "dbe564efcd5f03a9ea8f",
+    "url": "/kimyoungjo-portfolio/static/css/main.0b7ca9ee.chunk.css"
+  },
+  {
+    "revision": "2076ff1de717fb87bbf1",
+    "url": "/kimyoungjo-portfolio/static/js/2.18ee84e8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/kimyoungjo-portfolio/static/js/2.18ee84e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe564efcd5f03a9ea8f",
+    "url": "/kimyoungjo-portfolio/static/js/main.54397138.chunk.js"
+  },
+  {
+    "revision": "9498dd822b275f3f9d42",
+    "url": "/kimyoungjo-portfolio/static/js/runtime-main.f18f9cb2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kimyoungjo-portfolio/precache-manifest.b506e315905d79a6aec7901febc7cf88.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kimyoungjo-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
